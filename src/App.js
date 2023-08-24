02df8e891489f268dc95b4066d4e1f480e1f1813
Nosakeys <silaskeys95@gmail.com>
2023-08-25 00:58:29 +0100

    routers.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+
+
+
+
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path=" /" element= {</>} />
+          <Route path=" /cart"  element= {</>}/>
+        </Routes>
+      </Router>
     </div>
   );
 };
